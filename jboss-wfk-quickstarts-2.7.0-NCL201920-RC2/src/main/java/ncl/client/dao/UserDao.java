afb207c8f9dcf3a5440bc9d4890a9a81fbe70266
Chao Zhai <dichao1020@163.com>
2020-03-10 01:12:19 +0000

    create user files

---
+++ jboss-wfk-quickstarts-2.7.0-NCL201920-RC2/src/main/java/ncl/client/dao/UserDao.java
@@ -0,0 +1,11 @@
+package ncl.client.dao;
+
+/**
+ * @ClassName UserDao
+ * @Description: TODO
+ * @Author BENY
+ * @Date 2020/3/10
+ * @Version V1.0
+ **/
+public class UserDao {
+}
---
+++ jboss-wfk-quickstarts-2.7.0-NCL201920-RC2/src/main/java/ncl/client/service/UserService.java
@@ -0,0 +1,11 @@
+package ncl.client.service;
+
+/**
+ * @ClassName UserService
+ * @Description: TODO
+ * @Author BENY
+ * @Date 2020/3/10
+ * @Version V1.0
+ **/
+public class UserService {
+}
